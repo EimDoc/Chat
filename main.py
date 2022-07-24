3c8043f5a28a1611d10a9a21af0d5d11e9761d41
Efim Docenko <docenkoefim08@gmail.com>
2022-07-24 18:59:42 +0300

    'some'

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import subprocess
-string = 'sdfsdfdsfg'
-with open('chat.txt', 'a', encoding='utf-8') as file:
-    file.write(string + '\n')
 
-subprocess.call(['git', 'add', '.'])
+name = 'Caня'
+while True:
+    subprocess.call(['git', 'pull', 'https://github.com/EimDoc/Chat.git'])
+    string = input('Сообщение: ')
+    with open('chat.txt', 'a', encoding='utf-8') as file:
+        file.write(f'{name} - {string}\n')
+    subprocess.call(['git', 'add', '.'])
+    subprocess.call(['git', 'commit', '-m', "'some'"])
+    subprocess.call(['git', 'push', 'https://github.com/EimDoc/Chat.git'])
