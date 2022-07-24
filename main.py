dac92662dd659dba84a5a9c491fd8763599a6b37
Efim Docenko <docenkoefim08@gmail.com>
2022-07-24 19:04:05 +0300

    'some'

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ import subprocess
 
 name = 'Caня'
 while True:
-    subprocess.call(['git', 'pull', 'https://github.com/EimDoc/Chat.git'])
     string = input('Сообщение: ')
     with open('chat.txt', 'a', encoding='utf-8') as file:
         file.write(f'{name} - {string}\n')
     subprocess.call(['git', 'add', '.'])
     subprocess.call(['git', 'commit', '-m', "'some'"])
+    subprocess.call(['git', 'pull', 'https://github.com/EimDoc/Chat.git'])
     subprocess.call(['git', 'push', 'https://github.com/EimDoc/Chat.git'])
